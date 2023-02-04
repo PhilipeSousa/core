e95a322491980f0c5d766a48e94c3915bfcd301d
Philipe de Sousa <78869177+PhilipeSousa@users.noreply.github.com>
2023-02-03 23:00:06 -0300

    Update enum.py

--- homeassistant/backports/enum.py
+++ homeassistant/backports/enum.py
@@ -6,30 +6,19 @@ from typing import Any, TypeVar
 
 _StrEnumSelfT = TypeVar("_StrEnumSelfT", bound="StrEnum")
 
-
 class StrEnum(str, Enum):
-    """Partial backport of Python 3.11's StrEnum for our basic use cases."""
+    def __str__(self):
+        return self.value
 
     def __new__(
         cls: type[_StrEnumSelfT], value: str, *args: Any, **kwargs: Any
     ) -> _StrEnumSelfT:
-        """Create a new StrEnum instance."""
+
         if not isinstance(value, str):
-            raise TypeError(f"{value!r} is not a string")
+            raise TypeError(f"{value!r} não é uma string")
         return super().__new__(cls, value, *args, **kwargs)
 
-    def __str__(self) -> str:
-        """Return self.value."""
-        return str(self.value)
-
-    @staticmethod
     def _generate_next_value_(
         name: str, start: int, count: int, last_values: list[Any]
     ) -> Any:
-        """
-        Make `auto()` explicitly unsupported.
-
-        We may revisit this when it's very clear that Python 3.11's
-        `StrEnum.auto()` behavior will no longer change.
-        """
-        raise TypeError("auto() is not supported by this implementation")
+        raise TypeError("auto() não é suportado por essa implementação")
